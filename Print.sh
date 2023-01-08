5b6b1b1f2a99318f41c49ec7514ca89fca0eaa51
Mounika B <basetti.mounika@gmail.com>
2023-01-08 15:04:49 +0000

    Shell Scripting Topics

--- Print.sh
+++ Print.sh
@@ -3,3 +3,13 @@ echo ---- Hello----
 echo * hello
 
 echo "$ hello"
+
+
+# Colour Code Syntax : echo -e "\e[COLmMESSAGE\[0M"
+
+echo -e "\e[31mMESSAGE\[0M"
+echo -e "\e[32mMESSAGE\[0M"
+echo -e "\e[33mMESSAGE\[0M"
+echo -e "\e[34mMESSAGE\[0M"
+echo -e "\e[35mMESSAGE\[0M"
+echo -e "\e[36mMESSAGE\[0M"
